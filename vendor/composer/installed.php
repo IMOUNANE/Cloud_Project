1bb65ed7dc9a5454e527583dd20e07e9a24f69e1
gravity-zero <gravity.neo@gmail.com>
2021-03-27 14:33:41 +0100

    gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'abe3351f330fd538e23768862de318b7b2fe363c',
+    'name' => 'gravity/cloud_project',
+  ),
+  'versions' => 
+  array (
+    'gravity/cloud_project' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'abe3351f330fd538e23768862de318b7b2fe363c',
+    ),
+  ),
+);
