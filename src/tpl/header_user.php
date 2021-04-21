9bc15eb15fdea2e12ad74bd4afd958924c6b288a
Ismail Imounane <ismail95190@gmail.com>
2021-04-21 20:46:12 +0200

    add front back office

---
+++ src/tpl/header_user.php
@@ -0,0 +1,33 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $_GET['url'] ? $title  : 'Homepage' ;?></title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/header.css" />
+    <link rel="stylesheet" href="css/backoffice.css" />
+    <link rel="stylesheet" href="css/config/fonts.css" />
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <script defer src="js/burgerMenu.js"></script>
+  </head>
+  <body>
+
+  <div class="header-container">
+    <nav class="nav header-element" id="nav">
+      <div class="nav-open" id="open">
+          <div class="nav-openIcon"></div>
+      </div>
+    </nav>
+    
+    <div class="menu-logo element header-element">
+      <div>RGPD</div>
+    </div>
+
+    <div class="user element header-element">
+      <img class="user-icon" src="assets/icon/user-icon.svg" alt="">
+      <button class="button"><a href="index.php?url=logout">Déconnexion</a><img class="chevron" src="assets/icon/chevron-white.svg" alt=""></button>
+    </div>
+  </div>
