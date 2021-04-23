ce97250abeffa1628597ffc8d31be2e17a7af6f9
gravity-zero <gravity.neo@gmail.com>
2021-04-23 11:41:50 +0200

    push console.log loaded script

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../vendor/autoload.php';
-session_save_path("./tmp");
-session_start();
-
-setcookie("_ga", "", time()-3600);
-setcookie("exercice5", "", time()-3600);
-setcookie("PHPSESSID", "", time()-3600);
-require_once '../src/index.php';
