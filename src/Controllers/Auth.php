77a4e968949eea7f624eca0c4c7e4e7673d4098e
gravity-zero <gravity.neo@gmail.com>
2021-04-23 11:45:09 +0200

    modif

--- src/Controllers/Auth.php
+++ src/Controllers/Auth.php
@@ -205,7 +205,7 @@ class Auth{
 								script = document.createElement("script")
 								script.setAttribute("src", "'.$client_script_1.'")
 								body.appendChild(script)
-								console.log('.$client_script_1.' successfuly loaded);
+								console.log("'.$client_script_1.' successfuly loaded");
 							}
 							if(parseInt(choices.user_choice_2, 10) === 1){
 
@@ -213,7 +213,7 @@ class Auth{
 								script = document.createElement("script")
 								script.setAttribute("src", "'.$client_script_2.'")
 								body.appendChild(script)
-								console.log('.$client_script_2.' successfuly loaded);
+								console.log("'.$client_script_2.' successfuly loaded");
 							}
 							if(parseInt(choices.user_choice_3, 10) === 1){
 
@@ -221,7 +221,7 @@ class Auth{
 								script = document.createElement("script")
 								script.setAttribute("src", "'.$client_script_3.'")
 								body.appendChild(script)
-								console.log('.$client_script_3.' successfuly loaded);
+								console.log("'.$client_script_3.' successfuly loaded");
 							}
 							if(parseInt(choices.user_choice_4, 10) === 1){
 
@@ -229,7 +229,7 @@ class Auth{
 								script = document.createElement("script")
 								script.setAttribute("src", "'.$client_script_4.'")
 								body.appendChild(script)
-								console.log('.$client_script_4.' successfuly loaded);
+								console.log("'.$client_script_4.' successfuly loaded");
 							}
 						}
 				});
